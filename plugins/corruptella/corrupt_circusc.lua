3558e2ef2af96b030b345e573f19a1589daa7311
viti95 <victornt95@gmail.com>
2019-01-21 23:25:03 +0100

    Added Circus Charlie support (RAM, ROM, sound, video)

---
+++ plugins/corruptella/corrupt_circusc.lua
@@ -0,0 +1,74 @@
+local corClass = {	
+
+}
+
+function corClass.start()
+
+end
+
+function corClass.dump(filename)
+	dumpMEM(filename .. "_1", ":maincpu", 8)
+	dumpMEM(filename .. "_2", ":audiocpu", 8)
+end
+
+function corClass.restore(filename)
+	restoreMEM(filename .. "_1", ":maincpu", 8)
+	restoreMEM(filename .. "_2", ":audiocpu", 8)
+end
+
+function corClass.fuckRAM(mode)
+	if (mode == 0) then
+		fuckADDR(":maincpu", "program", 12, 8, 8)
+		fuckADDR(":maincpu", "program", 17, 8, 8)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":maincpu", "program", 12, 8, 1)
+		fuckADDR(":maincpu", "program", 17, 8, 1)
+	end
+end
+
+function corClass.fuckROM(mode)
+	if (mode == 0) then
+		fuckMEM(":maincpu", 8, 16)
+	end
+
+	if (mode == 1) then
+		fuckMEM(":maincpu", 8, 1)
+	end
+end
+
+function corClass.fuckVideo(mode)
+	if (mode == 0) then
+		fuckADDR(":maincpu", "program", 15, 8, 8)
+		fuckADDR(":maincpu", "program", 16, 8, 8)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":maincpu", "program", 15, 8, 1)
+		fuckADDR(":maincpu", "program", 16, 8, 1)
+	end
+end
+
+function corClass.fuckSound(mode)
+	if (mode == 0) then
+		fuckADDR(":audiocpu", "program", 2, 8, 8)
+		fuckMEM(":audiocpu", 8, 4)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":audiocpu", "program", 2, 8, 1)
+		fuckMEM(":audiocpu", 8, 1)
+	end
+end
+
+function corClass.fuckMusic(mode)
+
+end
+
+function corClass.drawHUD()
+
+end
+
+return corClass
+
