86c14d13c289bcc6ec2ff82b77a42abe9cdfc7ae
viti95 <victornt95@gmail.com>
2019-01-26 22:29:11 +0100

    Added Arkanoid 2 - Revenge of Doh support (RAM, ROM).
    Missing subcpu corruption.

---
+++ plugins/corruptella/corrupt_arknoid2.lua
@@ -0,0 +1,56 @@
+local corClass = {	
+
+}
+
+function corClass.start()
+
+end
+
+function corClass.dump(filename)
+	dumpMEM(filename .. "_1", ":maincpu", 8)
+	dumpMEM(filename .. "_2", ":sub", 8)
+end
+
+function corClass.restore(filename)
+	restoreMEM(filename .. "_1", ":maincpu", 8)
+	restoreMEM(filename .. "_2", ":sub", 8)
+end
+
+function corClass.fuckRAM(mode)
+	if (mode == 0) then
+		fuckADDR(":maincpu", "program", 5, 8, 8)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":maincpu", "program", 5, 8, 1)
+	end
+end
+
+function corClass.fuckROM(mode)
+	if (mode == 0) then
+		fuckMEM(":maincpu", 8, 16)
+	end
+
+	if (mode == 1) then
+		fuckMEM(":maincpu", 8, 1)
+	end
+end
+
+function corClass.fuckVideo(mode)
+
+end
+
+function corClass.fuckSound(mode)
+
+end
+
+function corClass.fuckMusic(mode)
+
+end
+
+function corClass.drawHUD()
+
+end
+
+return corClass
+
