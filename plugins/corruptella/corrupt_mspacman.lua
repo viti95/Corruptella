64166a56401b2c413b5ebf9dfcdb29a08fc99275
viti95 <victornt95@gmail.com>
2019-03-02 00:39:22 +0100

    Added Ms. PacMan support (RAM, ROM)

---
+++ plugins/corruptella/corrupt_mspacman.lua
@@ -0,0 +1,56 @@
+local corClass = {	
+
+}
+
+function corClass.start()
+
+end
+
+function corClass.dump(filename)
+	dumpMEM(filename .. "_1", ":maincpu", 8)
+end
+
+function corClass.restore(filename)
+	restoreMEM(filename .. "_1", ":maincpu", 8)
+end
+
+function corClass.fuckRAM(mode)
+	if (mode == 0) then
+		fuckADDR(":maincpu", "program", 6, 8, 8)
+		fuckADDR(":maincpu", "program", 7, 8, 8)
+	end
+	
+	if (mode == 1) then
+		fuckADDR(":maincpu", "program", 6, 8, 1)
+		fuckADDR(":maincpu", "program", 7, 8, 1)
+	end
+end
+
+function corClass.fuckROM(mode)
+	if (mode == 0) then
+		fuckMEM(":maincpu", 8, 16)
+	end
+
+	if (mode == 1) then
+		fuckMEM(":maincpu", 8, 1)
+	end	
+end
+
+function corClass.fuckVideo(mode)
+
+end
+
+function corClass.fuckSound(mode)
+
+end
+
+function corClass.fuckMusic(mode)
+
+end
+
+function corClass.drawHUD()
+
+end
+
+return corClass
+
