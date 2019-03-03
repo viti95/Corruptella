24cac745695ad2f5f4df4dfe8ffca1c35663bc67
viti95 <victornt95@gmail.com>
2019-03-03 13:21:30 +0100

    Added Bullfight support (RAM, ROM, sound)

---
+++ plugins/corruptella/corrupt_bullfgt.lua
@@ -0,0 +1,66 @@
+local corClass = {	
+
+}
+
+function corClass.start()
+
+end
+
+function corClass.dump(filename)
+	dumpMEM(filename .. "_1", ":maincpu", 8)
+	dumpMEM(filename .. "_2", ":soundcpu", 8)
+end
+
+function corClass.restore(filename)
+	restoreMEM(filename .. "_1", ":maincpu", 8)
+	restoreMEM(filename .. "_2", ":soundcpu", 8)
+end
+
+function corClass.fuckRAM(mode)
+	if (mode == 0) then
+		fuckADDR(":maincpu", "program", 3, 8, 8)
+		fuckADDR(":maincpu", "program", 4, 8, 8)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":maincpu", "program", 3, 8, 1)
+		fuckADDR(":maincpu", "program", 4, 8, 1)
+	end
+end
+
+function corClass.fuckROM(mode)
+	if (mode == 0) then
+		fuckMEM(":maincpu", 8, 16)
+	end
+
+	if (mode == 1) then
+		fuckMEM(":maincpu", 8, 1)
+	end
+end
+
+function corClass.fuckVideo(mode)
+
+end
+
+function corClass.fuckSound(mode)
+	if (mode == 0) then
+		fuckADDR(":soundcpu", "program", 2, 8, 8)
+		fuckMEM(":soundcpu", 8, 4)
+	end
+
+	if (mode == 1) then
+		fuckADDR(":soudncpu", "program", 2, 8, 1)
+		fuckMEM(":soundcpu", 8, 1)
+	end
+end
+
+function corClass.fuckMusic(mode)
+
+end
+
+function corClass.drawHUD()
+
+end
+
+return corClass
+
